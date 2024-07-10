8af1aa9d28fecdfdb707a480e8afb2ac2fbab600
Diogo Gaspar <diogo.marques.gaspar@tecnico.ulisboa.pt>
2024-07-10 21:56:46 +0100

    add round 2

--- app/_examples.ts
+++ app/_examples.ts
@@ -2,23 +2,24 @@ import { Category } from "./_types";
 
 export const categories: Category[] = [
   {
-    category: 'ALBUNS - SALVADOR SOBRAL',
-    items: ["EXCUSE ME", "BPM", "PARIS, LISBOA", "TIMBRE"],
+    category: 'MÚSICAS SAMPLADAS POR ARTISTAS AMERICANOS',
+    items: ["AMOR TOTAL", "A SOMBRA (FADO NOCTURNO)", "TOUCH ME", "TODO O MUNDO É NINGUÉM"],
     level: 1,
   },
   {
-    category: "PREMIOS PLAY - MELHOR ALBUM (2021-2024)",
-    items: ["COR D'AGUA", "CASA GUILHERMINA", "70 VOLTAS AO SOL", "KRIOLA"],
+    category: "DEBUT ALBUMS - MEMBROS GROGNATION",
+    items: ["DEEPAK LOOPER", "INDIANA JONES", "ADRENALINA", "MALVERDE"],
     level: 2,
   },
   {
-    category: "MUSICAS DE ARTISTAS QUE TOCARAM NO PRIMAVERA 2024",
-    items: ["1970", "COISAS FRACAS", "CANSADA", "ROUBAR UM CORPO"],
+    category: "ÁLBUNS/EP'S COM UMA MÚSICA CHAMADA \"INTERLÚDIO\"",
+    items: ["#FFFFFF", "INACABADO", "REFLEXO", "OITAVO CÉU"],
     level: 3,
   },
   {
-    category: "SINGLES DO T-REX QUE NAO ESTAO EM PROJETOS (EP/ALBUM/ETC)",
-    items: ["SALLY", "TOPO DO GAME", "TEMPO", "FEELING"],
+    category: "MÚSICAS SEGUNDO LUGAR FESTIVAL DA CANÇÃO (1970-1973)",
+    items: ["CANÇÃO DE MADRUGAR", "FLOR SEM TEMPO", "VAMOS CANTAR DE PÉ", "É POR ISSO QUE EU VIVO"],
     level: 4,
   },
 ];
+
