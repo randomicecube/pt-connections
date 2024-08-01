3e0cee376870eff1e11954764816ea5586e6ec8b
Diogo Gaspar <diogo@gaspa.pt>
2024-08-01 13:50:10 +0100

    add round 4

--- app/_examples.ts
+++ app/_examples.ts
@@ -2,23 +2,23 @@ import { Category } from "./_types";
 
 export const categories: Category[] = [
   {
-    category: "ALBUNS LANÇADOS A 9 DE MAIO",
-    items: ["STADIUM ARCADIUM", "THE NEW AMERICA", "EYES OPEN", "ELECTRONIC SOUND"],
+    category: "ARTISTAS PORTUGUESES QUE JÁ TOCARAM/VÃO TOCAR EM ESTOCOLMO",
+    items: ["MARO", "MARIZA", "MOONSPELL", "CARMINHO"],
     level: 1,
   },
   {
-    category: "MÚSICAS QUE (HOPEFULLY) VAO PASSAR NO PAREDES",
-    items: ["FOGO", "NASNUSA", "VARIAS QUEIXAS", "FAVOURITE"],
+    category: "ARTISTAS COM MúSICAS NO TOP15 +STREAMS SPOTIFY PORTUGAL",
+    items: ["BÁRBARA BANDEIRA", "D.A.M.A", "IVANDRO", "T-REX"],
     level: 2,
   },
   {
-    category: "MÚSICAS COM NOMES DE SUPER HEROIS (OU VILOES)",
-    items: ["BRUCE WAYNE", "BANE", "IRON MAN", "BLACK WIDOW"],
+    category: "MÚSICAS ONDE O TÍTULO TEM UM NÚMERO",
+    items: ["00:26", "2:22", "11915", "CXV"],
     level: 3,
   },
   {
-    category: "MÚSICAS QUE TAMBEM SAO NOMES DE FILMES",
-    items: ["FANDANGO", "MARVIN'S ROOM", "TARZAN", "FALLEN LEAVES"],
+    category: "ÚLTIMAS 3 MÚSICAS QUE SAÍRAM NO CANAL DA THINK MUSIC :(",
+    items: ["MARFIM", "DAMN/SKY", "IMORTAIS", "TYPE R"],
     level: 4,
   },
 ];
